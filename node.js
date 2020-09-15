ac4dadb0a7e0181edba5cfdabeb9c0abd60f3937
CBanks <banksc7669@yahoo.com>
2020-09-15 17:03:08 -0400

    formatted

---
+++ node.js
@@ -0,0 +1,11 @@
+document.getElementById("home").onclick = function () {
+    location.href = "index.html";
+};
+
+document.getElementById("aboutus").onclick = function () {
+    location.href = "contact.html";
+};
+
+document.getElementById("portfolio").onclick = function () {
+    location.href = ".html";
+};
